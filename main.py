c0239845a038fba486588e8080c7acd86be08fee
alex-phillips <ahp118@gmail.com>
2020-10-24 08:23:27 -0400

    support for output directory

--- main.py
+++ main.py
@@ -16,6 +16,11 @@ parser.add_argument(
     help="The location of the configuration file",
     default=os.path.expanduser("~/.tinc/config"),
 )
+parser.add_argument(
+    "--output",
+    help="Output directory for the config files",
+    default=None,
+)
 args = parser.parse_args()
 
 args.config = args.config.rstrip('/')
@@ -34,6 +39,8 @@ host_config = net_config['hosts'][args.hostname]
 
 net_location = net_config['dir'] if 'dir' in net_config else '/etc/tinc/{}'.format(
     args.network)
+if args.output is not None:
+    net_location = args.output
 
 if not os.path.exists(net_location):
     os.makedirs(net_location)
@@ -55,9 +62,29 @@ ip link set $INTERFACE down
 st = os.stat("{}/tinc-down".format(net_location))
 os.chmod("{}/tinc-down".format(net_location), st.st_mode | stat.S_IXUSR | stat.S_IXGRP | stat.S_IXOTH)
 
+if not os.path.exists("{}/hosts".format(net_location)):
+    os.makedirs("{}/hosts".format(net_location))
+
 host_list = []
 for hostname in net_config['hosts']:
     host_list.append("ConnectTo = {}".format(hostname))
+
+    if hostname != args.hostname:
+        print("{} - {}".format(hostname, args.hostname))
+        remote_host_key = "{}/hosts/{}".format(net_location, hostname)
+        if not os.path.exists(remote_host_key):
+            print("Creating remote host file at hosts/{}".format(hostname))
+            remote_host_conf = []
+            if 'address' in net_config['hosts'][hostname]:
+                remote_host_conf.append("Address = {}".format(net_config['hosts'][hostname]['address']))
+            if 'subnet' in net_config['hosts'][hostname]:
+                remote_host_conf.append("Subnet = {}".format(net_config['hosts'][hostname]['subnet']))
+            if 'public_key' in net_config['hosts'][hostname]:
+                remote_host_conf.append("")
+                remote_host_conf.append("{}".format(net_config['hosts'][hostname]['public_key']))
+            with open("{}/hosts/{}".format(net_location, hostname), "w") as handle:
+                handle.write("\n".join(remote_host_conf))
+
 host_list = "\n".join(host_list)
 
 print("Creating tinc.conf...")
@@ -67,9 +94,6 @@ Interface = {interface}
 AddressFamily = {address_family}
 {host_list}
 """.format(hostname=args.hostname, interface=net_config['interface'], address_family=net_config['address_family'], host_list=host_list))
-
-if not os.path.exists("{}/hosts".format(net_location)):
-    os.makedirs("{}/hosts".format(net_location))
 
 host_key = "{}/hosts/{}".format(net_location, args.hostname)
 if not os.path.exists(host_key):
